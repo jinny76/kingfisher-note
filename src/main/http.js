5209625d4910119bb8a1760f88b92841a040c564
jinni <jinni@ventuz.com.cn>
2024-05-10 18:14:42 +0800

    fix: fix resource path

--- src/main/http.js
+++ src/main/http.js
@@ -27,7 +27,7 @@ export const initHttpServer = () => {
   if (isDevelopment) {
     server.use('/vditor', express.static('resources/vditor'));
   } else {
-    server.use('/vditor', express.static('../../Resources/vditor'));
+    server.use('/vditor', express.static(app.getAppPath() + '../../Resources/vditor'));
   }
 
   server.listen(13999, () => log.log(
